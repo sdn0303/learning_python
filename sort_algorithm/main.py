42dc15fde9e713cb5e558681eee0fb46eb615542
sdn0303 <sdn0303.guitar@gmail.com>
2020-03-31 15:09:11 +0900

    add insert sort

--- sort_algorithm/main.py
+++ sort_algorithm/main.py
@@ -4,15 +4,15 @@
 def bubble_sort(numbers):
     for idx in reversed(range(len(numbers))):
         for i in range(idx):
-            if numbers[i] > numbers[i+1]:
-                numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+            if numbers[i] > numbers[i + 1]:
+                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
     return numbers
 
 
 def selection_sort(numbers):
     for idx in range(len(numbers)):
         mini = idx
-        for i in range(idx+1, len(numbers)):
+        for i in range(idx + 1, len(numbers)):
             if numbers[mini] > numbers[i]:
                 mini = i
         tmp = numbers[mini]
@@ -21,11 +21,34 @@ def selection_sort(numbers):
     return numbers
 
 
+def insert_sort(numbers):
+    for idx in range(len(numbers)):
+        for i in reversed(range(idx)):
+            if numbers[i] > numbers[i + 1]:
+                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+            else:
+                break
+
+    return numbers
+
+
+def heap_sort(numbers):
+    heap = set(numbers)
+    
+
+
+
 if __name__ == "__main__":
-    result = bubble_sort(numbers=[9, 2, 1, 5, 7, 3, 8, 4, 6, 0])
+    numbers_list = [9, 2, 1, 5, 7, 3, 8, 4, 6, 0]
+
+    result = bubble_sort(numbers_list)
     print(f"Bubble Sort: {result}")
     print("------------------------")
 
-    result = selection_sort(numbers=[9, 2, 1, 5, 7, 3, 8, 4, 6, 0])
+    result = selection_sort(numbers_list)
     print(f"Selection Sort: {result}")
+    print("------------------------")
+
+    result = insert_sort(numbers_list)
+    print(f"Insert Sort: {result}")
     print("------------------------")
